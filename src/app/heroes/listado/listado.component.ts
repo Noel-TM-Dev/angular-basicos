eed70b180a6a5f3bdcccdb6e0bd8c314d936c0f2
Noel Trejo <noeltrejom16@gmail.com>
2021-06-15 08:43:35 -0600

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+
+})
+export class ListadoComponent{
+   heroes: string[] = ['Spiderman','IronMan', 'Hulk', 'Thor'];
+   heroeBorrado: string = "";
+
+   borrarHeroe(){
+       this.heroeBorrado = this.heroes.shift() || '';
+       //console.log("El heroe borrado es:", heroeBorrado);
+   }
+
+  
+}
